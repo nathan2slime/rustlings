13a8060420d9902770c743004c1a1eb4d9a0b78a
aqua2boss <nathan3boss@gmail.com>
2024-02-12 01:37:00 -0300

    feat: initial commit

---
+++ exercises/iterators/iterators2.rs
@@ -0,0 +1,77 @@
+// iterators2.rs
+//
+// In this exercise, you'll learn some of the unique advantages that iterators
+// can offer. Follow the steps to complete the exercise.
+//
+// Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
+// hint.
+
+// Step 1.
+// Complete the `capitalize_first` function.
+// "hello" -> "Hello"
+
+pub fn capitalize_first(input: &str) -> String {
+    let mut c = input.chars();
+
+    let first = match c.next() {
+        None => String::new(),
+        Some(first) => first.to_string(),
+    };
+
+    input.replace(&first, &first.to_uppercase())
+}
+
+// Step 2.
+// Apply the `capitalize_first` function to a slice of string slices.
+// Return a vector of strings.
+// ["hello", "world"] -> ["Hello", "World"]
+pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
+    let mut res_inter = vec![];
+
+    words.iter().for_each(|word|
+        res_inter.push(capitalize_first(word))
+    );
+
+    res_inter
+}
+
+// Step 3.
+// Apply the `capitalize_first` function again to a slice of string slices.
+// Return a single string.
+// ["hello", " ", "world"] -> "Hello World"
+pub fn capitalize_words_string(words: &[&str]) -> String {
+    let mut value = String::new();
+
+    words.iter().for_each(|word|
+        value.push_str(&capitalize_first(word))
+    );
+
+    value
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_success() {
+        assert_eq!(capitalize_first("hello"), "Hello");
+    }
+
+    #[test]
+    fn test_empty() {
+        assert_eq!(capitalize_first(""), "");
+    }
+
+    #[test]
+    fn test_iterate_string_vec() {
+        let words = vec!["hello", "world"];
+        assert_eq!(capitalize_words_vector(&words), ["Hello", "World"]);
+    }
+
+    #[test]
+    fn test_iterate_into_string() {
+        let words = vec!["hello", " ", "world"];
+        assert_eq!(capitalize_words_string(&words), "Hello World");
+    }
+}
