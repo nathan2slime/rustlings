4cdc572884b084137f18d0b3cb0783e26b9163b7
aqua2boss <nathan3boss@gmail.com>
2024-02-12 23:17:10 -0300

    feat: init structs

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -5,13 +5,13 @@
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 struct ColorClassicStruct {
-    // TODO: Something goes here
+    red: i32,
+    green: i32,
+    blue: i32
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+struct ColorTupleStruct(i32, i32, i32);
 
 #[derive(Debug)]
 struct UnitLikeStruct;
